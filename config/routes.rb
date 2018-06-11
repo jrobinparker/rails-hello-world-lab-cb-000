55a2ffe37f02a37806c7eef450d1f1fdb6762a02
Jeremy Parker <jrobinparker@gmail.com>
2018-06-11 19:56:01 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+<<<<<<< HEAD
   get "hello_world", to: "hello#hello_world"
+=======
+  get "hello_world#hello"
+>>>>>>> 801be30cb8cc8211f1924c6dc07e6be2a555bb09
   
 end
